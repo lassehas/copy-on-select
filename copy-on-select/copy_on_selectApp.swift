54ca56d155f8ce991ebf868f41f94133e7dfa4e3
lassehas <haslundlasse@icloud.com>
2026-06-16 11:01:00 +0200

    Initial Commit

---
+++ copy-on-select/copy_on_selectApp.swift
@@ -0,0 +1,17 @@
+//
+//  copy_on_selectApp.swift
+//  copy-on-select
+//
+//  Created by Lasse Haslund on 16/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct copy_on_selectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
